49f2b58d548dda92eb1469a41e57c8e6f725587e
Lane Whitmore <lwhitmore@sandiego.edu>
2022-10-13 11:01:05 -0700

    Update rent_app.py

--- rent_app.py
+++ rent_app.py
@@ -3,12 +3,12 @@ from explore_page import show_explore_page
 from prediction_page import show_prediction_page
 
 ## Open both pages with css style
-#with open('style.css') as f:
-    #st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-page = st.sidebar.selectbox("Predict Rent or Explore Options", ("Predict", "Explore"))
+    page = st.sidebar.selectbox("Predict Rent or Explore Options", ("Predict", "Explore"))
 
-if page == "Predict":
-    show_prediction_page()
-else:
-    show_explore_page()
+    if page == "Predict":
+        show_prediction_page()
+    else:
+        show_explore_page()
